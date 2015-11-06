15accdacf82ea91b92087c740c35b012f56f21d9
otiai10 <otiai10@gmail.com>
2015-11-06 15:49:18 +0900

    Fix cli

--- amesh/main.go
+++ amesh/main.go
@@ -2,54 +2,27 @@
 package main
 
 import (
-	"encoding/json"
-	"flag"
 	"fmt"
-	"log"
+	"image"
 	"net/http"
 
 	"github.com/otiai10/amesh"
-	"github.com/otiai10/query"
-)
+	"github.com/otiai10/gcat"
 
-var (
-	port  = "4010"
-	serve = false
+	_ "image/gif"
 )
-
-func init() {
-	flag.BoolVar(&serve, "s", false, "run the server")
-	flag.StringVar(&port, "p", port, "port of server")
-	flag.Parse()
-}
 
 func main() {
-	if serve {
-		server()
-	}
-}
-
-func server() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ResponseJSON(w, amesh.GetEntry(), r)
-	})
-	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
-	log.Fatalln(err)
-}
-
-// ResponseJSON ...
-func ResponseJSON(w http.ResponseWriter, v interface{}, r *http.Request) {
-	var b []byte
-	var err error
-	if query.Bool(r, "pretty", false) {
-		b, err = json.MarshalIndent(v, "", "\t")
-	} else {
-		b, err = json.Marshal(v)
-	}
+	entry := amesh.GetEntry()
+	res, err := http.Get(entry.Mesh)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(fmt.Sprintf(`{"message":"%s"}`, err.Error())))
-		return
+		panic(err)
 	}
-	w.Write(b)
+	defer res.Body.Close()
+	img, _, err := image.Decode(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	gcat.OfTerminal().PrintImage(img)
+	fmt.Println("#amesh", entry.URL)
 }

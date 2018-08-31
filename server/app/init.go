a5511ae60ef601fc358b177f1a3778dd8770bc9d
Hiromu OCHIAI <otiai10@gmail.com>
2018-08-31 16:48:22 +0900

    meshiコマンドプラグインの実装

--- server/app/init.go
+++ server/app/init.go
@@ -14,6 +14,7 @@ func init() {
 
 	p := []plugins.Plugin{
 		plugins.Image{GoogleAPIKey: os.Getenv("GOOGLE_API_KEY"), GoogleCustomSearchEngineID: os.Getenv("GOOGLE_CUSTOM_SEARCH_ENGINE_ID")},
+		plugins.Meshi{YelpAPIKey: os.Getenv("YELP_API_KEY")},
 	}
 
 	s := services.Handler(p...)

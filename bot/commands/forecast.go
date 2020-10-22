50e89351128a626efa241ba3a58e40d9d5d9fa01
Hiromu OCHIAI <otiai10@gmail.com>
2020-10-22 12:46:47 +0900

    予報の表示の修正

--- bot/commands/forecast.go
+++ bot/commands/forecast.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"os"
 	"time"
 
@@ -28,41 +29,45 @@ func (cmd ForecastCommand) Handle(ctx context.Context, payload *slack.Payload) s
 	if err != nil {
 		return slack.Message{Channel: payload.Event.Channel, Text: err.Error()}
 	}
-	res, err := client.ByCityName("Tokyo", nil)
+	city := "Tokyo"
+	res, err := client.ByCityName(city, nil)
 	if err != nil {
 		return slack.Message{Channel: payload.Event.Channel, Text: err.Error()}
 	}
 	if len(res.Forecasts) == 0 || len(res.Forecasts[0].Weather) == 0 {
 		return slack.Message{Channel: payload.Event.Channel, Text: "Not enough forecast entries."}
 	}
+
 	message := slack.Message{
 		Channel: payload.Event.Channel,
-		Blocks:  []slack.Block{},
+		Text:    city,
 	}
 
-	// {{{ 日付で分けて、Blockを作っていく
+	// {{{ 日付で分けて、行をつくっていく
 	var blockdate int
-	var block *slack.Block = nil
-	for _, forecast := range res.Forecasts {
-		_, month, date := time.Unix(forecast.Timestamp, 0).In(loc).Date()
+	placeholder := cmd.getPlaceholderEmoji()
+	for i, forecast := range res.Forecasts {
+		w := forecast.Weather[0]
+		t := time.Unix(forecast.Timestamp, 0).In(loc)
+		_, month, date := t.Date()
 		// 新しい日付であればBlockを初期化
 		if date != blockdate {
-			if block != nil {
-				message.Blocks = append(message.Blocks, *block)
+			message.Text += "\n"
+			message.Text += fmt.Sprintf("%d/%d ", month, date)
+			if t.Hour() != 0 {
+				for h := 0; h < t.Hour(); h += 3 {
+					message.Text += placeholder
+				}
 			}
-			block = &slack.Block{Type: "context", Elements: []slack.Element{{Type: "plain_text", Text: fmt.Sprintf("%d/%d", month, date)}}}
 			blockdate = date
 		}
-		w := forecast.Weather[0]
-		block.Elements = append(
-			block.Elements,
-			// Element{Type: "plain_text", Text: time.Unix(forecast.Timestamp, 0).In(loc).Format("15:04")},
-			slack.Element{Type: "image", ImageURL: w.IconURL("@2x"), AltText: w.Description},
-			// Element{Type: "plain_text", Text: "|"},
-		)
-	}
-	if block != nil {
-		message.Blocks = append(message.Blocks, *block)
+		emoji := cmd.convertOpenWeatherMapIconToSlackEmoji(w.Icon)
+		message.Text += emoji
+		if i == len(res.Forecasts)-1 && t.Hour() != 21 {
+			for h := t.Hour() + 3; h < 24; h += 3 {
+				message.Text += placeholder
+			}
+		}
 	}
 	// }}}
 
@@ -75,4 +80,34 @@ func (cmd ForecastCommand) Help(payload *slack.Payload) slack.Message {
 		Channel: payload.Event.Channel,
 		Text:    "天気予報コマンド",
 	}
+}
+
+func (cmd ForecastCommand) convertOpenWeatherMapIconToSlackEmoji(icon string) string {
+	// https://openweathermap.org/weather-conditions
+	dictionary := map[string]string{
+		"01": ":sunny:",
+		"02": ":mostly_sunny:",
+		"03": ":partly_sunny:",
+		"04": ":cloud:",
+		"09": ":rain_cloud:",
+		"10": ":partly_sunny_rain:",
+		"11": ":thunder_cloud_and_rain:",
+		"13": ":snowflake:",
+		"50": ":fog:",
+	}
+	if emoji, ok := dictionary[icon[:2]]; ok {
+		return emoji
+	}
+	return cmd.getPlaceholderEmoji()
+}
+
+func (cmd ForecastCommand) getPlaceholderEmoji() string {
+	candidates := []string{
+		":marijuana:",
+		":white_small_square:",
+		":shrimp:",
+		":slack:",
+		":sunglasses:",
+	}
+	return candidates[rand.Intn(len(candidates))]
 }

327a36e67bee1b6645768d25108b9b54dd270c00
Hiromu OCHIAI <otiai10@gmail.com>
2020-10-23 10:52:52 +0900

    天気予報の土日はboldにした

--- bot/commands/forecast.go
+++ bot/commands/forecast.go
@@ -131,12 +131,12 @@ func (cmd ForecastCommand) getPlaceholderEmoji() string {
 
 func (cmd ForecastCommand) getJapaneseWeekday(day time.Weekday) string {
 	return map[time.Weekday]string{
-		time.Sunday:    "日",
+		time.Sunday:    "*日*",
 		time.Monday:    "月",
 		time.Tuesday:   "火",
 		time.Wednesday: "水",
 		time.Thursday:  "木",
 		time.Friday:    "金",
-		time.Saturday:  "土",
+		time.Saturday:  "*土*",
 	}[day]
 }
